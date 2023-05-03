4f16ded4698fe6fd4d6845bf709a05c083da7673
salocinc <nicolascalbucuravillalobos@gmail.com>
2023-05-02 22:42:08 -0400

    comentarios en algoritmo de permutación

--- algoritmo_permutacion.cpp
+++ algoritmo_permutacion.cpp
@@ -4,81 +4,64 @@
 #include <algorithm>
 #include <random>
 using namespace std;
+
+// inverse_perm: int[] x int -> int[]
 // Calcula la inversa de una permutacion
 // Ejemplo:
-// input: 2 3 4 0 1
-// output:3 4 0 1 2
+// Input: 2 3 4 0 1
+// Output:3 4 0 1 2
 int *inverse_perm(int perm[], int n){
     int *inv_perm = (int *) malloc(n*sizeof(int));
     for(int i=0; i<n; i++){
         inv_perm[perm[i]-1] = i+1;
     }
     return inv_perm;
-}  
+}
 
+// Testeo de la funcion inverse_perm
 void mainforcase (int n){
-    //int n = 4;    
-    //int arr[n] = {3, 2, 1, 4, 5, 6, 7, 8, 9, 10};
-    
-    //int n = int(pow(2,20)); // size of array
+    //Se crea un arreglo de tamaño n
     int *arr = new int[n];
-    // Fill array with 1 to n con std::iota
+
+    //Se llena el arreglo con los numeros del 1 al n
     std::iota(arr, arr + n, 1);
 
-    // Shuffle the array randomly
+    //Se permuta el arreglo de manera aleatoria
     std::shuffle(arr, arr + n, std::mt19937{std::random_device{}()});
-
-
-    /*
-    para debuguear
-    //Se printea el arreglo input como un arreglo al estilo [1 2 ...]
-    cout << "[ ";
-    for(int i=0; i<n; i++){
-        cout << arr[i] << " ";
-    }
-    cout << "]" << endl;*/
 
     //Se inicia el cronometro
     auto inicio = chrono::high_resolution_clock::now();
-    //Se calcula la permutacion inversa
+
+    //Se calcula la permutación inversa
     int *ip = inverse_perm(arr, n);
-
-    /*
-    para debuguear
-    //se printea la permutacion inversa como un arreglo al estilo [1 2 ...]
-    cout << "[ ";
-    for(int i=0; i<n; i++){
-        cout << ip[i] << " ";
-    }
-    cout << "]" << endl;*/
-
-
-
 
     //Se finaliza el cronometro
     auto fin = chrono::high_resolution_clock::now();
 
-    //escribe en un archivo llamado resultados_algoritmo_permutacion.txt
-    //el elemento inv_perm[perm[n-1]-1] y el elemento perm[n-1]
-    FILE *f = fopen("resultados_algoritmo_permutacion.txt", "a");
-    fprintf(f, "Para n = %d\n", n);
-    fprintf(f, "El elemento inv_perm[perm[n-1]-1] es %d y el elemento perm[n-1] es %d\n", ip[arr[n-1]-1], arr[n-1]);
-    //ademas se guardan los tiempos en el archivo
-    fprintf(f, "La funcion tardo %ld milisegundos en ejecutarse.\n\n", std::chrono::duration_cast<std::chrono::milliseconds>(fin - inicio).count());
-    fclose(f);
-
-
     //Se calcula el tiempo transcurrido
-    auto duracion = std::chrono::duration_cast<std::chrono::milliseconds>(fin - inicio).count();
+    auto duracion = chrono::duration_cast<chrono::milliseconds>(fin - inicio).count();
+
+    //Se abre el archivo donde se guardarán los tiempos
+    FILE *f = fopen("resultados_algoritmo_permutacion.txt", "a"); //append
+
+    //Se escribe el tiempo en el archivo
+    fprintf(f, "La funcion tardo %ld milisegundos en ejecutarse.\n\n", duracion);
+
+    fclose(f);
+    
     cout << "La funcion tardo " << duracion << " milisegundos en ejecutarse." << endl;
     free(ip);
     delete[] arr;
 }
 
 int main(){
+    //Se crea el archivo donde se guardaran los resultados
+    FILE *f = fopen("resultados_algoritmo_permutacion.txt", "w+");
+    fclose(f);    
+    
     int n;
     //testeamos de 2^20 a 2^30
-    for(int i=30; i<=30; i++){
+    for(int i=22; i<=22; i++){
         n = int(pow(2,i));
         cout << "Para n = " << n << endl;
         // testeamos 10 veces para el n dado
---
+++ merge_sort.cpp
@@ -0,0 +1,9 @@
+
+    /*
+    para debuguear
+    //Se printea el arreglo input como un arreglo al estilo [1 2 ...]
+    cout << "[ ";
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << "]" << endl;*/
